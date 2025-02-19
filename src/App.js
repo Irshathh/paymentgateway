00be72591caf29b2cfe29e7eaba5cb881a36f187
Irshathh <irshathmohamed01@gmail.com>
2025-02-19 16:02:51 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Razorpay from './Razorpay';
+// import Razorpay from './Razorpay';
 import Payment from './Payment';
 
 function App() {
   return (
     <div className="App">
-      <Razorpay/>
+      {/* <Razorpay/> */}
       <Payment/>
     </div>
   );
